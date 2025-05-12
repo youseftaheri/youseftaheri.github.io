378bd5ec719879e470e96bdc18269443dab04daf
youseftaheri <neuer.youseftaheri@gmail.com>
2025-05-12 21:41:51 +0330

    Correct repository name in next.congig.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/yousef-portfolio' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/youseftaheri' : '',
   images: { unoptimized: true }, // Required for static export
 };
 
