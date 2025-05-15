005ebd16dcff8a65e5f298ab58be6ab4246e4267
youseftaheri <neuer.youseftaheri@gmail.com>
2025-05-15 20:58:35 +0330

    Remove app folder

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/youseftaheri' : '',
+  // basePath: '/youseftaheri',
   images: { unoptimized: true }, // Required for static export
   trailingSlash: true, // Recommended for GitHub Pages
 };
